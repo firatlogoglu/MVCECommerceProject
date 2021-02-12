e2f3f760e2a0140bb1ced26f027d3eee52c40bed
firatlogoglu <firatlogoglu@gmail.com>
2021-02-12 15:50:19 +0300

    Kullanılmayan using'ler kaldırıldı

--- MVCECommerceProject.CORE/Entity/IEntity.cs
+++ MVCECommerceProject.CORE/Entity/IEntity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MVCECommerceProject.CORE.Entity
+﻿namespace MVCECommerceProject.CORE.Entity
 {
     public interface IEntity<T>
     {

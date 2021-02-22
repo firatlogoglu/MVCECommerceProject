f14d5628af4a4d9c4690d3f34cf16b59dc53e214
firatlogoglu <firatlogoglu@gmail.com>
2021-02-22 13:42:20 +0300

    Common katmanı oluşturuldu

---
+++ MVCECommerceProject.COMMON/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCECommerceProject.COMMON
+{
+    public class Class1
+    {
+    }
+}

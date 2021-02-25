7e21c821784b871ad20c555a24064d81101f1c13
firatlogoglu <firatlogoglu@gmail.com>
2021-02-25 17:17:48 +0300

    ManegementAuthFilter tanımlandı.

---
+++ MVCECommerceProject.MVC/Filters/AuthorizationFilters/ManegementAuthFilter.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace MVCECommerceProject.MVC.Filters.AuthorizationFilters
+{
+    public class ManegementAuthFilter : FilterAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (filterContext.HttpContext.Session["MLogin"] == null)
+            {
+                filterContext.Result = new RedirectResult("~/Manegement/Account/Login");
+            }
+        }
+    }
+}
